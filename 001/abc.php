7a6302c648ee3e95761fc98b13c8d19964897867
admin <tranthidathao_t62@hus.edu.vn>
2019-05-09 18:52:13 +0700

    234

---
+++ 001/abc.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 5/9/2019
+ * Time: 06:51 PM
+ */
+echo "abc";
---
+++ 001/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 5/9/2019
+ * Time: 06:32 PM
+ */
+echo "http://localhost/1901ephp/001/";
