b90485d051d0d6775b29228a00150233859e7c12
admin <tranthidathao_t62@hus.edu.vn>
2019-05-14 18:44:49 +0700

    234

--- 001.bientrongphp/index11.php
+++ 001.bientrongphp/index11.php
@@ -13,6 +13,14 @@ $str = "chao ha noi";
 echo "<br> Hàm strlen(biến) dùng để đếm số ký tự : " . strlen($str);
 echo "<br> Hàm str_word_count (biến) dùng để đếm số từ trong chuỗi : " . str_word_count($str);
 echo "<br> Hàm strrev(biến) dùng để đảo ngược chuỗi : " . strrev($str);
+
+echo "<br> Hàm strpos() :" .strpos("Chao ha noi","ha noi");
+echo "<br>";
+$x=strpos("chao ha noi","da nang");
+var_dump($x);
+echo "<br>";
+echo str_replace("ha noi","ho chi minh","chao thanh pho ha noi");
 ?>
+
 </body>
 </html>
